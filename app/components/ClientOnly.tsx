f961fd846b8433c732e481d13fbfa0222d242b2e
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-05-07 10:29:02 -0300

    ClientOnly component to wrap all components

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,24 @@
+//verificar se estamos usando server side rendering ou não
+
+import { useEffect, useState } from "react"
+
+const ClientOnly = ({children}: {children:React.ReactNode}) => {
+
+  const[hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() =>  {
+    setHasMounted(true)
+  },[])
+
+  if(!hasMounted){
+    return null;
+  }
+
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+export default ClientOnly
