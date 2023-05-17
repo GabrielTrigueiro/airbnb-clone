9808be2e7b995ae931caf8037c5effad18553068
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-05-17 12:58:18 -0300

    filters showing in serch

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,13 +1,50 @@
 'use client'
 
-import React from 'react';
+import React, {useMemo} from 'react';
 import { BiSearch } from 'react-icons/bi';
 
 import useSearchModal from '@/app/hooks/useSearchModal';
+import { useSearchParams } from 'next/navigation';
+import useCountries from '@/app/hooks/useCountries';
+import { differenceInDays } from 'date-fns';
 
 const Search = () => {
 
   const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const locationValue = params?.get('locationValue');
+  const startDate = params?.get('startDate');
+  const endDate = params?.get('endDate');
+  const guestCount = params?.get('guestCount');
+
+  const locationLabel = useMemo(() => {
+    if(locationValue){
+      return getByValue(locationValue as string)?.label;
+    }
+    return 'Anywhere';
+  },[getByValue, locationValue])
+
+  const durationLabel = useMemo(() => {
+    if(startDate && endDate){
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+      if(diff === 0){
+        diff = 1
+      }
+      return `${diff} Days`
+    }
+    return 'Any week'
+  },[startDate, endDate])
+
+  const guestLabel = useMemo(() => {
+    if(guestCount){
+      return `${guestCount} Guests`
+    }
+    return 'Add Guests'
+  },[guestCount])
 
   return (
     <div
@@ -34,7 +71,7 @@ const Search = () => {
                 font-semibold
                 px-6
             '>
-          Anywhere
+          {locationLabel}
         </div>
         <div className='
                 text-sm
@@ -46,7 +83,7 @@ const Search = () => {
                 flex-1
                 text-center
             '>
-          Any week
+          {durationLabel}
         </div>
         <div className='
                 text-sm
@@ -59,7 +96,7 @@ const Search = () => {
                 gap-3
             '>
           <div className='hidden sm:block'>
-            Add Guests
+            {guestLabel}
           </div>
           <div className='p-2 bg-rose-500 rounded-full text-white'>
             <BiSearch size={18} />
