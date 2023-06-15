cfed5929e086548b56e0ada34372d397e20412c8
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-06-15 12:40:52 -0300

    npm build working

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,78 +1,47 @@
-'use client';
 
-import { toast } from "react-hot-toast";
-import axios from "axios";
-import { useCallback, useState } from "react";
-import { useRouter } from "next/navigation";
+import EmptyState from "@/app/components/EmptyState";
+import ClientOnly from "@/app/components/ClientOnly";
 
-import { SafeReservation, SafeUser } from "@/app/types";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getReservations from "@/app/actions/getReservations";
 
-import Heading from "@/app/components/Heading";
-import Container from "@/app/components/Container";
-import ListingCard from "@/app/components/listings/ListingCard";
+import TripsClient from "./TripsClient";
 
-interface TripsClientProps {
-  reservations: SafeReservation[],
-  currentUser?: SafeUser | null,
-}
+const TripsPage = async () => {
+  const currentUser = await getCurrentUser();
 
-const TripsClient: React.FC<TripsClientProps> = ({
-  reservations,
-  currentUser
-}) => {
-  const router = useRouter();
-  const [deletingId, setDeletingId] = useState('');
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="Unauthorized"
+          subtitle="Please login"
+        />
+      </ClientOnly>
+    );
+  }
 
-  const onCancel = useCallback((id: string) => {
-    setDeletingId(id);
+  const reservations = await getReservations({ userId: currentUser.id });
 
-    axios.delete(`/api/reservations/${id}`)
-    .then(() => {
-      toast.success('Reservation cancelled');
-      router.refresh();
-    })
-    .catch((error) => {
-      toast.error(error?.response?.data?.error)
-    })
-    .finally(() => {
-      setDeletingId('');
-    })
-  }, [router]);
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No trips found"
+          subtitle="Looks like you havent reserved any trips."
+        />
+      </ClientOnly>
+    );
+  }
 
   return (
-    <Container>
-      <Heading
-        title="Trips"
-        subtitle="Where you've been and where you're going"
+    <ClientOnly>
+      <TripsClient
+        reservations={reservations}
+        currentUser={currentUser}
       />
-      <div 
-        className="
-          mt-10
-          grid 
-          grid-cols-1 
-          sm:grid-cols-2 
-          md:grid-cols-3 
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-        "
-      >
-        {reservations.map((reservation: any) => (
-          <ListingCard
-            key={reservation.id}
-            data={reservation.listing}
-            reservation={reservation}
-            actionId={reservation.id}
-            onAction={onCancel}
-            disabled={deletingId === reservation.id}
-            actionLabel="Cancel reservation"
-            currentUser={currentUser}
-          />
-        ))}
-      </div>
-    </Container>
-   );
+    </ClientOnly>
+  );
 }
  
-export default TripsClient;
+export default TripsPage;
